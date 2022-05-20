2a7c8a7999344d48f872def0e1a9db3fca3daa93
Jarod Florence <jarod.florence@outlook.com>
2022-05-19 19:36:32 -0500

    resolvers created

---
+++ server/schema/typeDef.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addBook(authors: [String], description: String, bookId: String, image: String, link: String, title: String): Book
+    removeBook (bookId: String)
+  }
+`;
+
+module.exports = typeDefs;
